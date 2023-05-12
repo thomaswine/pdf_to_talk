e0a9b92333f2266071abb5a46dcb9f0725d09b41
tboros1991 <tboros1991@gmail.com>
2023-05-12 19:47:12 +0200

    implement text to speech library

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-greetings = "Hello"
-name = "Tomi"
-print(greetings + name)
+from gtts import gTTS
+import os
+
+tts = gTTS(text="", lang="en")
+
+filename = "voice.mp3"
+tts.save(filename)
+os.system(f"start {filename}")
