d3b2beb4eb9b9086ff15b9d7fc9264a44a79af70
tboros1991 <tboros1991@gmail.com>
2023-05-12 20:44:36 +0200

    implement pypdf2 library

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from gtts import gTTS
+from PyPDF2 import PdfReader
 import os
 
-file = open("sample.txt", "r").read().replace("\n", " ")
+reader = PdfReader("sample.pdf")
 
-tts = gTTS(text=str(file), lang="fr")
+print(len(reader.pages))
+
+page = reader.pages[0]
+
+pdf_text = page.extract_text()
+
+print(pdf_text) 
+
+#file = open("sample_pdf.pdf", "r").read().replace("\n", " ")
+
+tts = gTTS(text=pdf_text, lang="fr")
 
 filename = "voice.mp3"
 tts.save(filename)
