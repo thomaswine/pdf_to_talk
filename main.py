9dfba50b5c2ba63cd034c95138dbdc9be0d22f63
tboros1991 <tboros1991@gmail.com>
2023-05-12 19:58:12 +0200

    read text from file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from gtts import gTTS
 import os
 
-tts = gTTS(text="", lang="en")
+file = open("sample.txt", "r").read().replace("\n", " ")
+
+tts = gTTS(text=str(file), lang="fr")
 
 filename = "voice.mp3"
 tts.save(filename)
