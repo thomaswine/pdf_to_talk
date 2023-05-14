a499a1935bd7f713ba8ffb54c59a48248ad5956b
tboros1991 <tboros1991@gmail.com>
2023-05-14 18:58:06 +0200

    implement class separation

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-from gtts import gTTS
-from PyPDF2 import PdfReader
-import os
+from text_manager import CreateText
+from speaker_manager import Speaker
 
-reader = PdfReader("sample.pdf")
-final_text = ""
-print(len(reader.pages))
 
-for page_number in range(len(reader.pages)):
-    page = reader.pages[page_number]
-    pdf_text = page.extract_text()
-    final_text += pdf_text
-    
-print(final_text)
+text = CreateText
+speaker = Speaker
 
-tts = gTTS(text=final_text, lang="en", slow=False)
-
-filename = "voice.mp3"
-tts.save(filename)
-os.system(f"start {filename}")
+final_text = text.create_text_from_pdf("sample.pdf")
+speaker.speaker(final_text, "en")
