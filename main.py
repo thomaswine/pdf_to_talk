4df86a9dcf86c63a8ff22bf00f88f956e19e9bb5
tboros1991 <tboros1991@gmail.com>
2023-05-14 19:35:55 +0200

    implement - reading top 10 movies in theatre

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from text_manager import CreateText
 from speaker_manager import Speaker
 
-
 text = CreateText
 speaker = Speaker
 
-final_text = text.create_text_from_pdf("sample.pdf")
+#final_text = text.create_text_from_pdf("sample.pdf")
+final_text = text.create_top_list_from_webpage("https://www.pastemagazine.com/movies/in-theaters/the-10-best-movies-in-theaters-right-now")
+print(final_text)
 speaker.speaker(final_text, "en")
