56604ef388a72ac921390cc0dd71c060cf875d82
tboros1991 <tboros1991@gmail.com>
2023-05-16 10:06:02 +0200

    implement path choosing

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from speaker_manager import Speaker
 text = CreateText
 speaker = Speaker
 
-input = int(input("What do you want to heart?"))
+input = int(input("What do you want to hear?\nPress 1 to read sample.pdf\nPress 2 to read top 10 movies in Netflix\nNumber:"))
 
 if input == 1:
     final_text = text.create_text_from_pdf("sample.pdf")
 elif input == 2:
-    #final_text = text.create_top_list_from_webpage("https://www.pastemagazine.com/movies/in-theaters/the-10-best-movies-in-theaters-right-now")
     final_text = text.create_top_list_from_webpage("https://www.pastemagazine.com/movies/netflix/popular-on-netflix-daily-top-10")
 
 print(final_text)
